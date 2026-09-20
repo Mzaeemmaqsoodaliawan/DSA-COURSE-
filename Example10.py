74df5c6dd67c9fe188208dc3773271064a05137d
MUHAMMAD ZAEEM <zaeemmaqsood.awan@gmail.com>
2026-09-20 10:23:25 +0500

    Add files via upload

---
+++ Example10.py
@@ -0,0 +1,6 @@
+def display(arr):
+    for i in arr:
+        print(i)
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+display(array)
---
+++ example04.py
@@ -0,0 +1,6 @@
+array = 0 * 10 #array of length 10 having all zeros
+print(array)
+#2D array having all zeros
+array1 = [[0 for x in range(4)] for y in range(3)]
+print(array1)
+
